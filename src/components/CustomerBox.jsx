eb035eaace1a9af3d4e0571e016d013ea1e9860e
lee14k <kmkovach@mtu.edu>
2024-01-26 10:03:11 -0500

    left padding on customer type box

--- src/components/CustomerBox.jsx
+++ src/components/CustomerBox.jsx
@@ -1,8 +1,9 @@
 import TripleRowBox from "./TripleRowBox"
 const CustomerBox =()=>{
     return (
-    <div className="triplerowwrapper my-24 ">
-        <div className="text-white my-24 ">
+        <div className="orangebg">
+    <div className="ml-24 my-24 ">
+        <div className="text-white my-24 orangebg">
   < TripleRowBox useTripleRowWrapper={true}
     mainheader={true}
 headOne="Small Businesses"
@@ -10,7 +11,10 @@ headTwo="Individuals"
 headThree="Corporations"
 spanOne="we love helping small businesses find their voice"
 spanTwo="family photo shoots? We've got you covered"
-spanThree="from headshots to professional branding, we do it all"/>
+spanThree="from headshots to professional branding, we do it all"
+className="orangebg ml-24"
+/>
+ </div>
  </div>
  </div>
     )
