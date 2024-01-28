31ba31e48b860385be6ac1b138b261a64089262a
lee14k <kmkovach@mtu.edu>
2024-01-27 21:54:44 -0500

    changing to side scroller

--- src/components/PortfolioBox.jsx
+++ src/components/PortfolioBox.jsx
@@ -72,12 +72,12 @@ export default function PortfolioBox (){
           },
         ],
       })
-return (
-    <div className="portfolio pt-48">
-        <h1 className="text-6xl big-headline ">Check out our work</h1>
-        <div className="w-full px-2 py-16 sm:px-0 ">
-      <Tab.Group>
-        <Tab.List className="flex rounded-xl bg-orange-900/20 p-1 mx-16">
+      return (
+        <div className="portfolio pt-48">
+            <h1 className="text-6xl big-headline">Check out our work</h1>
+            <div className="w-full px-2 py-16 sm:px-0">
+                <Tab.Group>
+                    <Tab.List className="flex rounded-xl bg-orange-900/20 p-1 mx-16">
           {Object.keys(categories).map((category) => (
             <Tab
               key={category}
@@ -94,44 +94,42 @@ return (
               {category}
             </Tab>
           ))}
-        </Tab.List>
-        <Tab.Panels className="mt-2">
-          {Object.values(categories).map((posts, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white/60 ring-offset-2 ring-offset-orange-400 focus:outline-none focus:ring-2'
-              )}
-            >
-              <ul className="grid grid-cols-3 pb-48">
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
-                  >
-                
+        </Tab.List>                 
+                    <Tab.Panels className="mt-2">
+                        {Object.values(categories).map((posts, idx) => (
+                            <Tab.Panel
+                                key={idx}
+                                className={classNames(
+                                    'rounded-xl bg-white p-3',
+                                    'ring-white/60 ring-offset-2 ring-offset-orange-400 focus:outline-none focus:ring-2'
+                                )}
+                            >
+                                <div className="flex overflow-x-auto pb-48">
+                                    {posts.map((post) => (
+                                        <div
+                                            key={post.id}
+                                            className="flex-none w-1/3 p-3"
+                                        >
+                                            <div
+                                                className="photoboxes"
+                                                style={{ backgroundImage: `url(${post.img})` }}
+                                            ></div>
 
-                <div
-                        className="photoboxes"
-                        style={{ backgroundImage: `url(${post.img})` }}
-                      ></div>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        'absolute inset-0 rounded-md',
-                        'ring-orange-400 focus:z-10 focus:outline-none focus:ring-2'
-                      )}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
-    </div>
-);
+                                            <a
+                                                href="#"
+                                                className={classNames(
+                                                    'absolute inset-0 rounded-md',
+                                                    'ring-orange-400 focus:z-10 focus:outline-none focus:ring-2'
+                                                )}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                            </Tab.Panel>
+                        ))}
+                    </Tab.Panels>
+                </Tab.Group>
+            </div>
+        </div>
+    );
 }
