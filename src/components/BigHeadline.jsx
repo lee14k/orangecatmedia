44d71140069748648097fa84f4050eaaf1f5e1a4
lee14k <kmkovach@mtu.edu>
2024-04-07 03:19:45 -0400

    trying to fix list and logo

--- src/components/BigHeadline.jsx
+++ src/components/BigHeadline.jsx
@@ -1,30 +1,25 @@
 import TripleRowBox from "./TripleRowBox";
 import Image from "next/image";
 const BigHeadline = () => (
-    <div>
-
-
+  <div>
     <div className="flex justify-center items-center">
-      <Image
-      src="/ocm.png"
-      height={600}
-      width={600}
-      />
+      <Image src="/back.png" height={600} width={600} />
     </div>
     <div className="my-24 ml-48  ">
-   < TripleRowBox useTripleRowWrapper={false}
-    mainheader={false}
-     headOne="15+"
-     headTwo="1.2M"
-     headThree="100K+"
-     spanOne="years of combined experience"
-     spanTwo="views on our work monthly"
-     spanThree="assets created"
-     headColor="#ed5724" // Custom color for heads
-     spanColor="black" // Custom color for spans
-     />
-      </div>
+      <TripleRowBox
+        useTripleRowWrapper={false}
+        mainheader={false}
+        headOne="15+"
+        headTwo="1.2M"
+        headThree="100K+"
+        spanOne="years of combined experience"
+        spanTwo="views on our work monthly"
+        spanThree="assets created"
+        headColor="#ed5724" // Custom color for heads
+        spanColor="black" // Custom color for spans
+      />
     </div>
+  </div>
 );
 
 export default BigHeadline;
--- src/components/CoolListEffect.jsx
+++ src/components/CoolListEffect.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const CoolListEffect = () => {
   const [translatePercentage, setTranslatePercentage] = useState(0);
@@ -10,7 +10,7 @@ const CoolListEffect = () => {
 
     // Map the scroll position (0 to scrollableHeight) to the percentage range (-43 to 15)
     const scrollFraction = scrollY / scrollableHeight;
-    return 15 - scrollFraction * 1.5*(2 - (-45));
+    return 15 - scrollFraction * 1.5 * (2 - -45);
   };
 
   const handleScroll = () => {
@@ -19,48 +19,59 @@ const CoolListEffect = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <div style={{ position: 'relative' }} className="overflow-hidden	" >
-      <div 
-        className="absolute-service-list" 
+    <div style={{ position: "relative" }} className="overflow-hidden	">
+      <div
+        className="absolute-service-list"
         style={{
-          willChange: 'transform',
+          willChange: "transform",
           transform: `translate3d(0px, ${translatePercentage}%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`,
-          transformStyle: 'preserve-3d'
+          transformStyle: "preserve-3d",
         }}
       >
-        Photos<br />
-        Videos<br />
-        Website Design<br />
-        Social Media<br />
+        Photos
+        <br />
+        Videos
+        <br />
+        Website Design
+        <br />
+        Social Media
+        <br />
         Marketing <br />
-        Branding<br />
-        + more
+        Branding
+        <br />+ more
       </div>
-      <div style={{
-        width: '100%',
-        height: '150px',
-        backgroundImage: 'linear-gradient(to bottom, white, rgba(255,255,255,0))',
-        position: 'absolute',
-        top: 0,
-        pointerEvents: 'none',
-      }} />
-      <div className="bottomgradient" style={{
-        height: '550px',
-        backgroundImage: 'linear-gradient(to top, white, rgba(255,255,255,0))',
-        position: 'absolute',
-        bottom: 0,
-        pointerEvents: 'none',
-        top:'300px',
-        width:"3500px",
-        zIndex:"100"
-      }} />
+      <div
+        style={{
+          width: "100%",
+          height: "150px",
+          backgroundImage:
+            "linear-gradient(to bottom, white, rgba(255,255,255,0))",
+          position: "absolute",
+          top: 0,
+          pointerEvents: "none",
+        }}
+      />
+      <div
+        className="bottomgradient"
+        style={{
+          height: "550px",
+          backgroundImage:
+            "linear-gradient(to top, white, rgba(255,255,255,0))",
+          position: "absolute",
+          bottom: 0,
+          pointerEvents: "none",
+          top: "35%",
+          width: "3500px",
+          zIndex: "100",
+        }}
+      />
     </div>
   );
 };
